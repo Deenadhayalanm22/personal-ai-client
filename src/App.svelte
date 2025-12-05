bb921f09f1920cf0c274102af696a65ca3840d66
Deenadhayalanm22 <53555317+Deenadhayalanm22@users.noreply.github.com>
2025-12-05 12:31:53 +0530

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+    let backendResponse = "";
+
+    async function callBackend() {
+        const res = await fetch("http://localhost:8080/transaction");
+        backendResponse = await res.text();
+    }
+</script>
+
+<h1>Backend Test</h1>
+<button on:click={callBackend}>Call Backend</button>
+
+<p>{backendResponse}</p>
